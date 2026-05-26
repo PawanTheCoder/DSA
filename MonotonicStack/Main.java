22425358b280732a8fe34ee85a3d284dd8ebfe1b
Pawan Fuke <fukeraju03@example.com>
2026-05-26 10:24:42 +0530

    Learned Basic Stack and Monotonic Stack Implementation

--- MonotonicStack/Main.java
+++ MonotonicStack/Main.java
@@ -1,7 +1,58 @@
 package MonotonicStack;
 
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
-        
+        int[] nums = { 2 , 2, 1 , 5 , 6 , 2 , 4 };
+        int[] result = nextGreater(nums);
+        for(int n : result) {
+            System.out.print(n + " ");
+        }
+        System.out.println();
+        result = nextSmaller(nums);
+        for(int n : result) {
+            System.out.print( n + " ");
+        }
+    }
+
+    public static int[] nextGreater(int[] nums) {
+        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[nums.length];
+
+        for(int i = 0 ; i < nums.length; i++) {
+
+            while(!stack.isEmpty() && nums[i] > nums[stack.peek()]) {
+                int index = stack.pop();
+                result[index] = nums[i];
+            }
+
+            stack.push(i);
+        }
+
+        while(!stack.isEmpty()) {
+            result[stack.pop()] = -1;  // Default Value for remaining indices
+        }
+        return result;
+    }
+
+    public static int[] nextSmaller(int[] nums) {
+        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[nums.length];
+
+        for(int i = 0 ; i < nums.length ; i ++) {
+             
+            while(!stack.isEmpty() && nums[i] < nums[stack.peek()]) {
+                int index = stack.pop();
+                result[index] = nums[i];
+            }
+
+            stack.push(i);
+        }
+
+        while(!stack.isEmpty()) {
+            result[stack.pop()] = -1;
+        }
+        return result;
     }
 }
---
+++ Stack/StackArray.java
@@ -0,0 +1,53 @@
+package Stack;
+
+public class StackArray {
+    private int capacity;
+    private int top;
+    private int[] arr;
+
+    StackArray(int size) {
+        capacity = size;
+        arr = new int[size];
+        top = -1;
+        capacity = size;
+    }
+
+    private boolean isEmpty() {
+        if(top != -1) return false;
+        return true;
+    }
+
+    public void push(int x) {
+        if(top == capacity - 1) {
+            System.out.println("Stack Overflow!");
+            return;
+        }else {
+            arr[++top] =  x;
+        }
+    }
+
+    public int pop() {
+        if(top == -1) {
+            System.out.println("Stack UnderFlow!");
+            return -1;
+        }
+        return arr[top --];
+    }
+
+    public int peek() {
+        if(isEmpty()) {
+            System.out.println("Stack Underflow!");
+            return -1;
+        }
+        return arr[top];
+    }
+
+    public void display() {
+        System.out.println("Stack : ");
+        System.out.println("  ___   ");
+        for(int i = top; i >=0 ; i--) {
+            System.out.println( " | " + arr[i] + " | ");
+        }
+        System.out.print("  ___  ");
+    }
+}
